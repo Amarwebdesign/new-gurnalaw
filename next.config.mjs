fbd1477282cd260939b83256690f6959cdaaf205
amrsoftec <hello@amrsoftec.com>
2026-03-23 10:43:20 +0530

    WORDPRESS_API_URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,3 @@
-if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
-  throw new Error(`
-    Please provide a valid WordPress instance URL.
-    Add to your environment variables WORDPRESS_API_URL.
-  `);
-}
-
-const { protocol, hostname, port, pathname } = new URL(
-  process.env.WORDPRESS_API_URL,
-);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -17,13 +6,11 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'secure.gravatar.com',
-        port,
         pathname: '/avatar/**',
       },
       {
         protocol: 'https',
         hostname: 'news.gurnalaw.ca',
-        port,
         pathname: '/wp-content/uploads/**',
       },
     ],
@@ -40,4 +27,3 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
