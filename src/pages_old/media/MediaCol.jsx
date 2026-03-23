1358da02b420cdab153db5401f8d2eff54998b6b
amrsoftec <hello@amrsoftec.com>
2026-03-23 13:47:14 +0530

    page-old

--- src/pages_old/media/MediaCol.jsx
+++
@@ -1,9 +0,0 @@
-export default function MediaCol({
-    src
-}) {
-    return (
-        <div className="flex justify-around  ">
-            <iframe className="w-[570px] h-[320px] border-8 border-[#fedb04]" src={src} frameorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-        </div>
-    );
-}
